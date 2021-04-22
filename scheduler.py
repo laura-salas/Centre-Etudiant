267788781389eb501df7238fb962897f7389a23a
Laura <laurasalas96@live.com>
2021-04-21 20:52:06 -0400

    Avoids duplicate classes happening at same time

--- scheduler.py
+++ scheduler.py
@@ -2,11 +2,15 @@ from datetime import datetime, timedelta
 from math import floor
 
 # Fr
-DAYS_OF_WEEK = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"]
-DOW_ABBREVIATIONS = ["Lun", "Ma", "Mer", "J", "V"]
-HOURS_PER_DAY = 16  # from 7 am to 11pm is the max
+
+# IF one wants the schedule to start or end at an earlier or later hour:
+HOURS_PER_DAY = 16  # from 7 am to 11pm is the max to make it look orderly
 START_TIME = 8  # 7am
 END_TIME = 19  # 11pm
+
+
+DAYS_OF_WEEK = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"]
+DOW_ABBREVIATIONS = ["Lun", "Ma", "Mer", "J", "V"]
 REF_DAY = datetime.fromisoformat("2021-04-18 00:00:00")
 C_CHAR = '|'  # column separator
 R_CHAR = '*'  # row separator
@@ -53,6 +57,9 @@ class Course:
                     return True
             return False
 
+        def __eq__(self, other):
+            return self.start == other.start and self.end == other.end
+
     def __init__(self, name: str):
         self.name = name  # example: 'Intro. Programming '
         self.slots = []  # for example, if there is a link lab to it
@@ -64,7 +71,8 @@ class Course:
         new_slot.end_str = end
         new_slot.date_time()
         new_slot.name = str(self)
-        self.slots.append(new_slot)
+        if new_slot not in self.slots:
+            self.slots.append(new_slot)
 
     def __eq__(self, other):
         return other.name == self.name
@@ -96,6 +104,7 @@ class Schedule:
         self.scheduleArray = []  # 2D string array where rows = hours and columns = days
         self.schedule = {}
         self.init_days()
+        self.IsSimplifiedCart = False
         self.parse_input(rawInput)
 
     def init_days(self):
@@ -139,23 +148,36 @@ class Schedule:
             day_idx += 1
 
     def parse_input(self, input_):
-        raw = input_.replace(COURSE_SEPARATOR_A, "#")
-        raw = raw.replace(COURSE_SEPARATOR_B, "#")
-        raw = raw.replace("\n", " ")
-        raw = raw.replace("\t", " ")
-        raw = raw.split("#")
-
         classes_str = []
 
-        for line in raw:
-            # SUPPRIMER -> first row, not counted
-            if not len(line) < 3 and "SUPPRIMER" not in line:
-                classes_str.append(line)
+        if COURSE_SEPARATOR_A not in input_:
+            raw = input_.replace("\n", " ")
+            raw = raw.replace("\t", " ")
+            self.IsSimplifiedCart = True
+            raw = raw.replace("Panier", "")
+            raw = raw.split(" ")
 
-        self.assign_dates_to_classes(classes_str)
+            self.organize_simplified_schedule(raw)
+
+        else:
+            raw = input_.replace(COURSE_SEPARATOR_A, "#")
+            raw = raw.replace(COURSE_SEPARATOR_B, "#")
+            raw = raw.replace("\n", " ")
+            raw = raw.replace("\t", " ")
+            raw = raw.split("#")
+
+
+            for line in raw:
+                # SUPPRIMER -> first row, not counted
+                if not len(line) < 3 and "SUPPRIMER" not in line:
+                    classes_str.append(line)
+
+            self.organize_table_schedule(classes_str)
+
+
         self.create_schedule()
 
-    def assign_dates_to_classes(self, arr):
+    def organize_table_schedule(self, arr):
         self.classes = []
         for line in arr:
             curr_word = None
@@ -175,11 +197,67 @@ class Schedule:
                     end = split_line[idx + 3]
                     self.classes[self.classes.index(curr_course)].add_slot(day_week, start, end)
 
-            # for course in self.classes:
-            #     print(course)
+
+    def organize_simplified_schedule(self, arr):
+        self.classes = []
+        splitted_lines = []
+        watching_idx = []
+        checking_for_class = True
+        reading_date = False
+        possible_class_name = ""
+
+        #check if int or not
+        def is_int(s):
+            try:
+                int(s)
+                return True
+            except ValueError:
+                return False
+
+        for word_idx in range(len(arr)):
+            word = arr[word_idx]
+            if word in DOW_ABBREVIATIONS:
+                reading_date = True
+            elif checking_for_class and not reading_date:
+                if possible_class_name:
+                    if is_int(word):
+                        if len(watching_idx) == 1:
+                            splitted_lines.append(arr[watching_idx[0]:word_idx-2])
+                        checking_for_class = False
+                        possible_class_name += word
+                        possible_class_name = ""
+                        watching_idx = [word_idx-1]
+                    else:
+                        possible_class_name = ""
+                else:
+                    possible_class_name += word
+            elif reading_date:
+                if (":") in word and "-" in arr[word_idx-1]:
+                    reading_date = False
+                    checking_for_class = True
+        splitted_lines.append(arr[watching_idx[0]:len(arr)])
+
+        for line in splitted_lines:
+            curr_word = None
+            curr_subject_name = line[0] + line[1]
+            if "-" in curr_subject_name:  # remove useless data
+                curr_subject_name = curr_subject_name[0:curr_subject_name.index("-")]
+            curr_course = Course(curr_subject_name)
+            if curr_course not in self.classes:  # don't add the same class twice
+                self.classes.append(curr_course)
+
+            for day in DOW_ABBREVIATIONS:
+                # if day in line:
+                while day in line:
+                    idx = line.index(day)
+                    day_week = DOW_ABBREVIATIONS.index(day)
+                    start = line[idx + 1]
+                    end = line[idx + 3]
+                    self.classes[self.classes.index(curr_course)].add_slot(day_week, start, end)
+                    line.remove(day)
 
     def pretty_print(self):
-        col_width = (7 + 2) * self.maxColLength  # 7 being the length of the class name
+        col_width = max((7 + 2) * self.maxColLength,(7 + 2) * 2)  # 7 being the length of the class name
         # todo: don't hardcode the length of the course name .
         heures = list(list(self.schedule.values())[0].keys())
         schedule_print = []
@@ -214,13 +292,14 @@ class Schedule:
                                             " " * (((col_width + 2 - len(lineToAdd)) // 2) - 2) + C_CHAR
 
         schedule_print.insert(0, day_line)
+        print("\t APERÇU DU PANIER: ")
         for line in schedule_print:
             print(line)
 
 
 if __name__ == "__main__":
-    print("Colle le screenshot ici. Voir screenshot pour savoir quoi selectionner. \n \
-          Après l'avoir collé, appuie sur enter deux fois. ")
+    print("Colle la selection du ici. Voir le README pour savoir quoi selectionner. \n \
+          Après l'avoir collé, appuie sur \"enter\"/\"retour\" 3-4 fois pour générer l'horaire. ")
     entered_schedule = ""
     noEmpties = 0
     while True:
